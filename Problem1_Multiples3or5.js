f56f48e0145518d9c8c96fd87c7fca54dcd762d2
ejtalavera <enrique.j.talavera@gmail.com>
2022-01-18 15:09:24 -0300

    Solución a problema 1

---
+++ Problem1_Multiples3or5.js
@@ -0,0 +1,28 @@
+target=999;
+
+function Multiples3or5(num) {
+    let i=num;
+    let sum=8;
+
+    for (;i>5;i--){
+        if (i%3===0)
+            sum+=i;
+        else
+            if (i%5===0)
+                sum+=i;
+    }
+
+    return sum;
+}
+
+Multiples3or5(target);
+
+//EULER SOLUTION
+function SumDivisibleBy(n) {
+    let p= target / n;
+
+    return n * (p* (p+1)) / 2;
+}
+
+//optimized answer
+SumDivisibleBy(3) + SumDivisibleBy(5) - SumDivisibleBy(15);
